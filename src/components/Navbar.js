0398a487488977673cdf4a4ac33afe78d4b160ac
prince475 <danielmwai475@gmail.com>
2023-03-13 12:25:44 +0300

    Showing nav image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
     import React from "react"
+    import logo from "../images/logo192.jpg"
 
 
 
     function Navbar() {
         return(
             <nav className="navbar">
-            <img className="image" src="../src/images/logo192.jpg" alt="React Facts" />
+            <img className="image" src={logo} alt="React Facts" />
+            <h2>React Facts</h2>
             <h3 className="nav-item">React Course-Project1 </h3>
             </nav>
         )
